966e8e7ed76824a0697aefaf15a97336cddc6c9d
null <null>
2025-07-08 10:46:29 +0900

    タスクの削除機能

--- src/main.js
+++ src/main.js
@@ -28,7 +28,8 @@ const onClickAdd = () => {
     const deleteButton = document.createElement('button');
     deleteButton.innerText = '削除';
     deleteButton.addEventListener('click', () => {
-        alert('delete');
+        const deleteTarget = deleteButton.closest('li');
+        document.getElementById('incomplete-list').removeChild(deleteTarget);
     })
 
     //liタグの子要素に各要素を設定
